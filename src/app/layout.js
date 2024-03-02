ae6f32a94cf08e549a367c19fa242315c793e759
KanishKumar11 <kanishkumar11@outlook.in>
2024-03-02 20:46:42 +0530

    title changed

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Happy Birthday Likitha",
+  title: "Wishes by Sampreet Kulkarni, A very happy birthday Likitha",
   description:
     "Birthday wishes by Sampreet Kulkarni, A very happy birthday Likitha",
 };
